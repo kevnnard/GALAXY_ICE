4577f719934b52baa54a22f029aa87f3ebfecd8e
kevnnard <londonokevin9@gmail.com>
2023-08-16 18:13:11 -0500

    borrar archivos de git

--- src/hooks/useRedux.tsx
+++
@@ -1,5 +0,0 @@
-import { AppDispatch, RootState, useSelector } from "@/store";
-import { TypedUseSelectorHook, useDispatch } from "react-redux";
-
-export const useAppSelector: TypedUseSelectorHook<RootState> = useSelector;
-export const useAppDispatch: () => AppDispatch = useDispatch;
